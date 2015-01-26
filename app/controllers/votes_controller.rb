ec26fa7821eefaa857aa52a5d597f5fcffbfd755
Alex <96dalex@gmail.com>
2015-01-26 08:08:17 -0700

    tests are passing yo

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,7 @@
 class VotesController < ApplicationController
   before_action :require_in_group, only: [:create]
   before_action :require_not_yet_voted, only: [:create]
+  before_action :require_current_user, only: [:create]
   def create
     train_option = TrainOption.find_by(id: params[:id])
     if train_option
@@ -25,5 +26,9 @@ class VotesController < ApplicationController
     if Vote.exists? user: current_user, train_option: params[:id]
       redirect_to :back, notice: "You've already voted"
     end
+  end
+
+  def require_current_user
+    redirect_to :back, notice: "You must be logged in" unless current_user
   end
 end
